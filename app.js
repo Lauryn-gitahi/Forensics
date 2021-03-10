cfc6a33e8cdf352168fca459740ed9466e63023f
Lauryn-gitahi <lauryn.gitahi@gmail.com>
2021-03-10 12:13:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// // let btn1= document.querySelector('.btn1');
+// let list=document.querySelector('.text-forensics');
+// // btn1.addEventListener('click',displayText)
+
+// let button =document.querySelectorAll('.btn');
+
+// button[i].addEventListener('click',dispatchText())
+// for(let i=0;i>button.length;i++){
+//     button[i].addEventListener('click',function(){
+//     if(button[i].classlist.contains('btn1')){
+        
+//             let newDiv= document.createElement('div');
+//             newDiv.classList.add('comp');
+//             let h2=document.createElement('h2');
+//             h2.innerHTML='Computer Forensics';
+//             let p=document.createElement('p');
+//             p.innerText='The Computer Forensic Analysis process is the methodical approach to examining digital media to establish factual information for civil or criminal matters.'
+
+//             h2.appendChild(p)
+//             newDiv.appendChild(h2)
+//             list.appendChild(newDiv)
+        
+//     }
+//     else{
+//         console.log('nope');
+// })
+    
+//     // }
+// }
+
+let button =document.querySelectorAll('.btn')
